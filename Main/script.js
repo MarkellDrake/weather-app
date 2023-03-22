fcfbf71aee694bfd39064075c162fafd45ec942f
markelldrake99@gmail.com <markelldrake99@icloud.com>
2023-03-21 19:18:39 -0700

    insert new API in order to display weather data

--- Main/script.js
+++ Main/script.js
@@ -1,7 +1,7 @@
 // Global variables
 var searchHistory = [];
 var weatherApiRootUrl = 'https://api.openweathermap.org';
-var weatherApiKey = 'd91f911bcf2c0f925fb6535547a5ddc9';
+var weatherApiKey = '085db3eb903faa4838e0cc8e3a272ff6';
 
 // DOM element references
 var searchForm = document.querySelector('#search-form');
